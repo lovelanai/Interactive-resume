652d52a2f8aade08ca256c426489da22309cce68
Love Lanai <love.lanai@medieinstitutet.se>
2021-11-29 02:13:08 +0100

    added content to resume

---
+++ main.js
@@ -0,0 +1,10 @@
+function changetext(text) {
+    const paragraph = document.getElementById("text")
+    paragraph.innerText = text;
+  }
+
+
+  function hej(){
+     
+    document.getElementById('chatbubble').style.display = "initial";
+}
