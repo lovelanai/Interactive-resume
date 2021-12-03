c0610b6d4aca2988aa997d0d118544746dab646f
Love Lanai <love.lanai@medieinstitutet.se>
2021-12-03 13:16:57 +0100

    almost finished product

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ sop.src = "./assets/audio/sop.wav";
 /** Removes intro-scene and replace it with the main content (resumé) */
 
 function showresume() {
+  //Remove intro scene
   var myobj = document.getElementById("container");
   myobj.remove();
   var myobj = document.getElementById("container-mobile");
   myobj.remove();
+
+  // Show main content (resumé)
   document.getElementById('resume').style.display = "grid";
-  $("#opencurtain").delay(3000).animate({"top": "-10rem"}, 500)
+
+  // Slide animation for curtain-opener
+  $("#opencurtain").delay(3000).animate({ "top": "-10rem" }, 500)
 }
 
 
@@ -38,32 +43,43 @@ document.onmousemove = function () {
 
 
 
-/**Curtain+effect */
+/**Curtain+effects */
 
 function open_curtain() {
+  // Starts zeppelin animation
   document.getElementById('zeppelin-container').style.display = "flex";
-  $("#arm").animate({"left": "-17.25rem", "z-index": "1"}, 100)
-  $("#arm").delay(400).animate({"top": "15rem"}, 200)
-  $("#arm").delay(100).animate({"top": "10rem","z-index":"-1"}, 100)
-  $("#arm").animate({"left": "0rem"}, 100)
+
+    // Arm pulling rope animation
+  $("#arm").animate({ "left": "-17.25rem", "z-index": "1" }, 100)
+  $("#arm").delay(400).animate({ "top": "15rem" }, 200)
+  $("#arm").delay(100).animate({ "top": "10rem", "z-index": "-1" }, 100)
+  $("#arm").animate({ "left": "0rem" }, 100)
+
+  // Curtain opening animation
 
   $("#curtain1").delay(500).animate({ width: 20 }, 1000);
   $("#curtain2").delay(500).animate({ width: 20 }, 1000);
-  $("#left-side").animate({"z-index": "1"}, 1400);
+  $("#left-side").animate({ "z-index": "1" }, 1400);
 
-
+// Switch between opening and closing curtain buttons
 
   document.getElementById('opencurtain').style.display = "none";
   document.getElementById('closecurtain').style.display = "initial";
 }
 function close_curtain() {
-  $("#arm").animate({"left": "-17.25rem", "z-index": "1"}, 100)
-  $("#arm").delay(400).animate({"top": "15rem"}, 200)
-  $("#arm").delay(100).animate({"top": "10rem","z-index":"-1"}, 100)
-  $("#arm").animate({"left": "0rem"}, 100)
+
+    // Arm pulling rope animation
+  $("#arm").animate({ "left": "-17.25rem", "z-index": "1" }, 100)
+  $("#arm").delay(400).animate({ "top": "15rem" }, 200)
+  $("#arm").delay(100).animate({ "top": "10rem", "z-index": "-1" }, 100)
+  $("#arm").animate({ "left": "0rem" }, 100)
+
+  // Curtain closing animation
   $("#curtain1").delay(500).animate({ width: 520 }, 1000);
   $("#curtain2").delay(500).animate({ width: 520 }, 1000);
-  $("#left-side").animate({"z-index": "0"}, 100);
+  $("#left-side").animate({ "z-index": "0" }, 100);
+
+  // Switch between closing and opening curtain buttons
   document.getElementById('opencurtain').style.display = "initial";
   document.getElementById('closecurtain').style.display = "none";
 }
